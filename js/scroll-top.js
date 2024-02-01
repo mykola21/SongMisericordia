bfb64f84cb59404a4e8b5bdac769d75a2c45237d
Mykola21 <kolasickotov@gmail.com>
2024-02-01 03:03:10 +0200

    btn edit

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 200) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("scrollToTopBtn").style.display = "block";
     } else {
         document.getElementById("scrollToTopBtn").style.display = "none";
